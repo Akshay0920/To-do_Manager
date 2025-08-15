7f5d025271784fbc75faab4c2df801387327ad3e
Akshay0920 <akshaysynam6@gmail.com>
2025-08-15 16:00:01 +0530

    Add all required tests and setup Prettier

--- todo.js
+++ todo.js
@@ -1,5 +1,55 @@
+/* eslint-disable no-undef */
 const todoList = () => {
   let all = [];
+  const todoList = () => {
+    const all = [];
+
+    const today = new Date().toISOString().split("T")[0];
+
+    const add = (todoItem) => {
+      all.push(todoItem);
+    };
+
+    const markAsComplete = (index) => {
+      all[index].completed = true;
+    };
+
+    const overdue = () => {
+      return all.filter((todo) => todo.dueDate < today);
+    };
+
+    const dueToday = () => {
+      return all.filter((todo) => todo.dueDate === today);
+    };
+
+    const dueLater = () => {
+      return all.filter((todo) => todo.dueDate > today);
+    };
+
+    const toDisplayableList = (list) => {
+      return list
+        .map(
+          (todo) =>
+            `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
+              todo.dueDate === today ? "" : todo.dueDate
+            }`,
+        )
+        .join("\n");
+    };
+
+    return {
+      all,
+      add,
+      markAsComplete,
+      overdue,
+      dueToday,
+      dueLater,
+      toDisplayableList,
+    };
+  };
+
+  module.exports = todoList;
+  const today = new Date().toISOString().split("T")[0];
 
   const add = (todoItem) => {
     all.push(todoItem);
